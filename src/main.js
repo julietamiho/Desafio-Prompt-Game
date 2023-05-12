33c56950505e283785d8ef419247ec6702b15b58
Julieta Miho Yamate <julietamiho@gmail.com>
2023-05-12 15:35:40 -0300

    enviando arquivo html

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// Elabore um programa que leia um numero e devolva para o usuário o dobro do número informado.
+let numero = prompt("informe o número")
+const converteNumero = parseInt(numero) 
+alert(converteNumero * 2)
