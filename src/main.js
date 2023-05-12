c808c85fee034fc3d2c05f0a8c3d0b434cb86f23
Julieta Miho Yamate <julietamiho@gmail.com>
2023-05-12 16:03:22 -0300

    resolvendo exercício 2

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@
 let numero = prompt("informe o número")
 const converteNumero = parseInt(numero) 
 alert(converteNumero * 2)
+
+// Elabore um programa que calcule o valor do jantar, sendo o valor total do jantar composto 
+// pela conta junto com a taxa de atendimento do garçom.
+const contaJantar = prompt("escreva o valor da sua conta")
+const taxaGarcom = parseInt(contaJantar * 0.10)
+const valorTotal = parseInt(contaJantar) + taxaGarcom
+alert("O valor total do jantar é: R$ " + valorTotal.toFixed(2))
